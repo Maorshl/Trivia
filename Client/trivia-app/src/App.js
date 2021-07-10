719284922dff48fa74260ff2daba52d7701ce554
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-07-10 17:21:20 +0300

    Update App.js

--- Client/trivia-app/src/App.js
+++ Client/trivia-app/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import Cookies from "js-cookie";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 axios.interceptors.response.use(
   (response) => {
